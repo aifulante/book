e0bc45e65f1207f0a37bd8c49b4868e8a8f779d6
finewenmu <101246525+finewenmu@users.noreply.github.com>
2022-05-20 09:38:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x ./bin/mdbook
 cd books/rust && ../../bin/mdbook clean && ../../bin/mdbook build
-cd ../../books/wiki && ../../bin/mdbook clean && ../../bin/mdbook build
+cd ../../books/graph && ../../bin/mdbook clean && ../../bin/mdbook build
